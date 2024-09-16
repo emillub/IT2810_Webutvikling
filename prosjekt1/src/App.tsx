f4584a8aa68bbcb5881bb55c3a795b1fa17081a7
Markuv <markuv@stud.ntnu.no>
2024-09-16 17:18:56 +0200

    Laget Routes i App

--- prosjekt1/src/App.tsx
+++ prosjekt1/src/App.tsx
@@ -1,16 +1,28 @@
 import './App.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import SwiperPage from './pages/swiperPage'
+import ListViewPage from './pages/listViewPage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
 
 const queryClient = new QueryClient()
 
 function App() {
 
+  const baseUrl: string = "/project1";
+
   return (
+    <>
       <QueryClientProvider client={queryClient}>
-        <SwiperPage/>
+        <Router>
+            <Routes>
+            <Route path={baseUrl } element={<Layout content={<SwiperPage />}/>} />
+            <Route path={baseUrl + "/listViewPage"} element={<Layout content={<ListViewPage/>}/>} />
+            </Routes>
+        </Router>
       </QueryClientProvider>
-  )
-}
+    </>
+    );
+  }
 
 export default App
